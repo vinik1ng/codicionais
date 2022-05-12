8fcf6185c54201efee35bb52e6992cd7f302cea2
vinik1ng <102757784+vinik1ng@users.noreply.github.com>
2022-05-12 14:31:00 -0300

    Add files via upload

---
+++ atividade.js
@@ -0,0 +1,44 @@
+//ativida 1
+
+let numero = prompt('Dos Números de 0 a 5. Em qual estou pensando?')
+
+if (numero == 4){
+    console.log('Você acertou. Parabéns!!')
+
+}else{
+    console.log('Você errou. que pena ')
+}
+
+
+//atividade 2
+let vel = prompt('Coloque a velocidade do carro');
+
+if (vel > 80){
+    let acima = (vel - 80)5;
+    console.log(Você esta acima do limite. A multa é de ${multa})
+}else{
+    console.log('Você esta dentro do limite')
+}
+
+
+//atividade 3 
+let num = prompt('Impar ou Par')
+
+if (num%2 == 0){
+    console.log('Par')
+}else{
+    console.log('Impar')
+}
+
+
+//atividade 4
+
+let salario = parseInt(prompt('Qual é o seu salario'));
+
+if (salario > 1250){
+    let aumento = (salario 10/100)+salario;
+    alert(aumento)
+}else{
+    let aumento2 = (salario * 15/100)+salario;
+    alert(aumento2)
+}
